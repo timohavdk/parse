8e126c95c852748181d0d52d872a9707d3133609
Timofey Andreev <andreev.ta1@dns-shop.ru>
2023-06-23 16:35:51 +1000

    Скрипт для парсинга данных по ПК hhru

--- main.py
+++ main.py
@@ -117,4 +117,53 @@ def main_loop(file_name):
     print('FINISH')
 
 
-main_loop(FILE_NAME)
+# main_loop(FILE_NAME)
+
+EMPLOYMENT_ID = 'employment_id'
+COUNT = 'count'
+
+HEADER_EMPLOYMENT_ID_COUNT = [
+    COUNT,
+    EMPLOYMENT_ID
+]
+
+
+def get_count_table_by_param(file_name, param, count, output_file_name, header):
+    array_of_dict = []
+
+    with open(file_name, 'r', encoding='utf-8', newline='') as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            if row[param] is not None:
+                is_exist = False
+
+                for current_row in array_of_dict:
+                    if current_row[param] == row[param]:
+                        is_exist = True
+
+                        current_row[count] += 1
+
+                if not is_exist:
+                    new_row = {
+                        param: row[param],
+                        count: 1
+                    }
+
+                    array_of_dict.append(new_row)
+
+    with open(output_file_name, 'w', encoding='cp1251', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        writer.writeheader()
+
+        writer.writerows(array_of_dict)
+
+
+get_count_table_by_param('./all-vacancy-with-area.csv', 'experience_name', COUNT,
+                         'count_vacancy_from_experience_name.csv',
+                         ['experience_name', COUNT])
+get_count_table_by_param('./all-vacancy-with-area.csv', 'employment_name', COUNT,
+                         'count_vacancy_from_employment_name.csv',
+                         ['employment_name', COUNT])
+get_count_table_by_param('./all-vacancy-with-area.csv', 'employer_name', COUNT, 'count_vacancy_from_employer_name.csv',
+                         ['employer_name', COUNT])
